1c36dd723d630a95efd896830d24a45039cdbe81
ZETYSX\Administrator <quyetnp@fsoft.com>
2018-05-20 14:16:51 +0700

    no message

--- app/src/main/java/com/gg/administrator/hci_project/MainActivity.java
+++ app/src/main/java/com/gg/administrator/hci_project/MainActivity.java
@@ -16,6 +16,7 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+		
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
